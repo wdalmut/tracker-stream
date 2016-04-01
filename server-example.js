c52081308edf5c5439a237403f78280e7f2e1696
Walter Dal Mut <walter.dalmut@gmail.com>
2016-04-01 19:52:51 +0200

    Show messages in the server example

--- server-example.js
+++ server-example.js
@@ -4,6 +4,6 @@ var Tk104Stream = require('.').Tk104Stream;
 
 net.createServer(function(client) {
   client.pipe(new Tk104Stream()).on('data', function(data) {
-    //console.log(JSON.parse(data));
+    console.log(JSON.parse(data));
   }).pipe(new Tk104Reply()).pipe(client);
 }).listen(9000);
